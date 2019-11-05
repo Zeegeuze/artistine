eb6406a2cff31ac8298d878ad189fefc89dde773
zeegeuze <wouter_verhaeghe@hotmail.com>
2019-11-05 17:45:18 +0100

    Eerste stap dbs

---
+++ app/admin/artwork.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Artwork do
+
+  permit_params :name, :description, :price
+  
+  
+  
+  index do
+    column :id
+    column :name
+    column :price do |cbl|
+      number_to_currency(cbl.price)
+    end
+    column :description
+    column :created_at
+  end
+  
+  show do
+    attributes_table do
+      row :name
+      row :price do |cbl|
+        number_to_currency(cbl.price)
+      end
+      row :description
+    end
+  end
+end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Artwork < ApplicationRecord
+
+
+  # def price(num)
+  #   @view.number_to_currency(num)
+  # end
+
 end
