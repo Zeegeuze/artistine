09d25648a9232022fec7774be2f3ded1611798c7
zeegeuze <wouter_verhaeghe@hotmail.com>
2020-03-01 20:22:43 +0100

    Verwijderen PhotosUploader wegens niet gebruikt

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -3,8 +3,6 @@
 class Artwork < ApplicationRecord
   has_many_attached :images
 
-  # mount_uploader :photo, PhotosUploader
-
   scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
 
   has_many :comments, inverse_of: :artwork
--- app/uploaders/photos_uploader.rb
+++
@@ -1,8 +0,0 @@
-# class PhotosUploader < CarrierWave::Uploader::Base
-# 
-#   include Cloudinary::CarrierWave
-# 
-#   CarrierWave.configure do |config|
-#    config.cache_storage = :file
-#   end
-# end
